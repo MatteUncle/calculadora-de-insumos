eabedc95d265e6c0c680fa02b736d4bd05cf54db
MatteUncle <148014576+MatteUncle@users.noreply.github.com>
2024-07-01 21:50:55 -0300

    Update # Peso médio de Cápsulas.py

--- # Peso médio de Cápsulas.py
+++ # Peso médio de Cápsulas.py
@@ -1,9 +1,11 @@
 # Peso médio de Cápsulas
 
-embalagens_peso = dict([ ("R30", 7.804), ("R60", 14.653), ("R90", 17.469), ("R120", 18.797), ("R180", 23.733),  
-                        ("R240", 30.032), ("R300", 34.946), ("R500", 60.282), ("R750", 67.397), ("R1000", 81.034),
-                          ("Env. grande", 1.3), ("Env. pequeno", 0.9)])
-capsula_peso = dict([("Cápsula 3", 0.05 ), ("Cápsula 2", 0.06), ("Cápsula 1", 0.075), ("Cápsula 0", 0.1), ("Cápsula 00", 0.12), ("Cápsula Dual", 0.15)])
+embalagens_peso = dict([ ("R30", 7.804), ("R60", 14.653), ("R90", 17.293), ("R120", 18.797), ("R180", 23.733),  
+                        ("R240", 30.032), ("R300", 34.946), ("R500", 60.282), ("R750", 67.397), ("R1000", 81.034), ("Nenhuma", 0.0)])
+
+capsula_peso = dict([("Cápsula 3", 0.05 ), ("Cápsula 2", 0.06), ("Cápsula 1", 0.075), ("Cápsula 0", 0.1), ("Cápsula 00", 0.12), ("Cápsula Dual", 0.15),
+                      ("Env. grande", 1.3), ("Env. pequeno", 0.9)])
+
 for potes in embalagens_peso:
     peso = embalagens_peso [potes]
 
@@ -13,16 +15,29 @@ for capsulas in capsula_peso:
 chave_embalagem = embalagens_peso.keys()
 chave_capsulas = capsula_peso.keys()
 
-tamanho_embalagem = input(f"Escolha o tamanho da embalagem:\n{chave_embalagem}\n").replace("env","Env.").capitalize()
-resultado_embalagem = embalagens_peso[tamanho_embalagem]
+tamanho_capsula = input(f"Escolha o tamanho da cápsula ou sachê:\n{chave_capsulas}\n").replace("capsula","Cápsula").replace("env","Env.").capitalize()
+resultado_capsula = capsula_peso[tamanho_capsula]
 
-tamanho_capsula = input(f"Escolha o tamanho da cápsula:\n{chave_capsulas}\n").replace("capsula","Cápsula")
-resultado_capsula =  capsula_peso[tamanho_capsula]
+if tamanho_capsula == "Env. grande" or tamanho_capsula == "Env. pequeno":
+    quantidade_sache = int(input("Qual é a quantidade de sachês?\n"))
+    sache_vazio = resultado_capsula * quantidade_sache
+
+    peso_inteiro = input("Qual é o total de todos os sachês?\n").replace(",",".")
+    peso_inteiro_novo = float(peso_inteiro)
+
+    peso_bruto = peso_inteiro_novo - sache_vazio
+    peso_medio_sache = peso_bruto / quantidade_sache
+
+    print(f"o peso total de máteria prima é: {peso_bruto:.2f}\nO peso médio dos sachês é: {peso_medio_sache:.3f}")
+    quit()
+
+tamanho_embalagem = input(f"Escolha o tamanho da embalagem:\n{chave_embalagem}\n").capitalize()
+resultado_embalagem = embalagens_peso[tamanho_embalagem]
 
 quantidade = int(input("Qual é a quantidade de cápsulas?\n"))
 cap_vazia = resultado_capsula * quantidade
 
-peso_completo = input("Qual é o peso total com pote e tampa?\n").replace(",",".")
+peso_completo = input("Qual é o peso total com pote e tampa ou sem embalagem?\n").replace(",",".")
 peso_completo_novo = float(peso_completo)
 peso_total =  peso_completo_novo - resultado_embalagem - cap_vazia
 
@@ -38,4 +53,4 @@ if silica == True:
 else:
     peso_medio = peso_total / quantidade
 
-print(f"O peso total de máteria prima é: {peso_total}\nO peso médio das cápsulas é: {peso_medio:.3f}")
+print(f"O peso total de máteria prima é: {peso_total:.2f}\nO peso médio das cápsulas é: {peso_medio:.3f}")
